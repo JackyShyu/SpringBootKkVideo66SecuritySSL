044c247ca1076c532d1d1cb0c9a50e5c5e63f21d
jackyshyu <jackyshyu@gmail.com>
2018-10-02 16:13:43 -0500

    add all files

---
+++ src/main/java/org/shyu/springboot/SpringBootKkVideo66SecuritySslApplication.java
@@ -0,0 +1,12 @@
+package org.shyu.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKkVideo66SecuritySslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKkVideo66SecuritySslApplication.class, args);
+	}
+}
